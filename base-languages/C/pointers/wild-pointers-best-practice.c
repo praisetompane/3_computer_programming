c24ed4d2a09998d5a47551b8cd29ef0904644bc6
Tompanepp <tompanepp@gmail.com>
2021-05-01 14:06:44 +0200

    static memory allocation

--- base-languages/C/pointers/wild-pointers-best-practice.c
+++ base-languages/C/pointers/wild-pointers-best-practice.c
@@ -4,7 +4,7 @@ int main() {
     int var;
     int *ptr = NULL; //Initialise it to a NULL pointer
     int *ptr2 = (int *)malloc(sizeof(int)); //Explicitly allocate memory to it
-    int ptr3 = &var; //Allocate it an existing variables memory address(Becareful with this one)
+    int *ptr3 = &var; //Allocate it an existing variables memory address(Becareful with this one)
 
     return 0;
 }
