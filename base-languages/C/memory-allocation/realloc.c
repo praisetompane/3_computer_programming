f0818724e9f9a833a9cf0738eb3a8eabbcd3fefc
Tompanepp <tompanepp@gmail.com>
2021-05-13 08:42:52 +0200

    do not override the first batch of numbers and introduce a concept of total number count

--- base-languages/C/memory-allocation/realloc.c
+++ base-languages/C/memory-allocation/realloc.c
@@ -8,11 +8,12 @@
 #include <stdlib.h>
 
 int main() {
- int numberOfNumbers, sum = 0;
+    int numberOfNumbers, totalNumbers, sum = 0;
 
     printf("Please enter number of numbers: ");
     scanf("%d", &numberOfNumbers);
 
+    totalNumbers = numberOfNumbers;
     int *numbers = (int *)malloc(sizeof(int) * numberOfNumbers);
     
     printf("Address stored in *numbers: %p\n", numbers);
@@ -22,12 +23,13 @@ int main() {
         exit(1);
     }
 
-    for(int i = 0; i < numberOfNumbers; i++) {
+    for(int i = 0; i < totalNumbers; i++) {
         printf("Please provide a number: ");
         scanf("%d", numbers + i);
     }
+    
     printf("Summing the numbers...\n");
-    for(int i = 0; i < numberOfNumbers; i++) {
+    for(int i = 0; i < totalNumbers; i++) {
         printf("Adding: %d\n", *(numbers + i));
         sum += *(numbers + i);
     }
@@ -36,7 +38,7 @@ int main() {
     printf("I have apetite for more summing :)\n");
     printf("Please provide another %d numbers\n", numberOfNumbers);
     
-    int totalNumbers = numberOfNumbers + numberOfNumbers;
+    totalNumbers = numberOfNumbers * 2;
     numbers = realloc(numbers, sizeof(int) * (totalNumbers));
 
     printf("Address stored in *numbers: %p\n", numbers);
@@ -44,19 +46,23 @@ int main() {
         printf("Failed to allocate additional memory, sorry :(\n");
         exit(1);
     }
-    for(int i = 0; i < numberOfNumbers; i++) {
+    
+    for(int i = numberOfNumbers; i < totalNumbers; i++) {
         printf("Please provide a number: ");
         scanf("%d", numbers + i);
     }
+
     printf("Summing again, yummy \n");
-    for(int i = 0; i < numberOfNumbers; i++) {
+    for(int i = numberOfNumbers; i < totalNumbers; i++) {
         printf("Adding: %d\n", *(numbers + i));
         sum += *(numbers + i);
     }
+
     printf("Here is the list of all the numbers \n");
     for(int i = 0; i < totalNumbers; i++) {
         printf("%d", *(numbers + i));
     }
+    
     printf("\n");    
     printf("Current sum of the numbers is: %d\n", sum);
     return 0;
