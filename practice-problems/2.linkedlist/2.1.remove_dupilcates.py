799060df8c4fb1ee843954f95d93b8b5c60b1f26
Tompanepp <tompanepp@gmail.com>
2021-06-06 20:55:30 +0200

    sample linkedlist and comment problem sets

---
+++ practice-problems/2.linkedlist/2.1.remove_dupilcates.py
@@ -0,0 +1,20 @@
+'''
+    Context:
+        Given unsorted linked list
+    Definitions:
+
+    Objective:
+        Remove duplicates
+    Assumptions:
+        Using singly linked list
+    Constraints:
+        None
+    Example(s):
+        input = [1, 2, 4, 6, 1, 6, 8, 5]
+        state:
+        
+    Algorithm flow:
+    
+
+
+'''
