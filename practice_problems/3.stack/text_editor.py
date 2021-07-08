6715815676b6b9f320bf2c1050d6204de846bcd3
Tompanepp <tompanepp@gmail.com>
2021-07-09 00:37:52 +0200

    improvements to editor interface and undo functionality to handle more than 1 character

--- practice_problems/3.stack/text_editor.py
+++ practice_problems/3.stack/text_editor.py
@@ -14,47 +14,32 @@ from impl.stack import Stack
         display:
             Time= O(1)
 '''
+
+
 class TextEditor:
     undo_stack = None
     redo_stack = None
     document = None
+
     def __init__(self, document):
         self.undo_stack = Stack()
         self.redo_stack = Stack()
         self.document = document
-        
-    def write(self, letter):
-        self.undo_stack.push(letter)
-        self.document += letter
+
+    def write(self, text):
+        self.undo_stack.push(text)
+        self.document += text
         self.display()
 
-    def ctrl_z(self):
-        self.undo_stack.pop()
-        self.redo_stack.push(self.document[-1])
-        self.document = self.document[:-1]
+    def undo(self):
+        self.redo_stack.push(self.undo_stack.peek())
+        last_word = self.undo_stack.pop()
+        self.document = self.document[:-len(last_word)]
         self.display()
 
-    def ctrl_shift_z(self):
+    def redo(self):
         self.document += self.redo_stack.pop()
         self.display()
 
     def display(self):
         print(self.document)
-
-def main():
-    print("Praise typing his body of knowldge")
-    word_processor = TextEditor("")
-    print("My blank slate")
-    word_processor.display()
-    word_processor.write("I")
-    word_processor.write(" ")
-    word_processor.write("k")
-    word_processor.write("m")
-    word_processor.ctrl_z()
-    word_processor.write("n")
-    word_processor.write("o")
-    word_processor.write("w")
-    print("Try undo and redo")
-    word_processor.ctrl_z()
-    word_processor.ctrl_shift_z()
-main()
