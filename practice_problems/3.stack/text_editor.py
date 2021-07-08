e298e9854fdae5b4258af36f393f2e0982469255
Tompanepp <tompanepp@gmail.com>
2021-07-08 21:16:34 +0200

    simple text editor using Stack

---
+++ practice_problems/3.stack/text_editor.py
@@ -0,0 +1,46 @@
+from impl.stack import Stack
+
+class TextEditor:
+    undo_stack = None
+    redo_stack = None
+    document = None
+    def __init__(self, document):
+        self.undo_stack = Stack()
+        self.redo_stack = Stack()
+        self.document = document
+        
+    def write(self, letter):
+        self.undo_stack.push(letter)
+        self.document += letter
+        self.display()
+
+    def undo(self):
+        self.undo_stack.pop()
+        self.redo_stack.push(self.document[-1])
+        self.document = self.document[:-1]
+        self.display()
+
+    def redo(self):
+        self.document += self.redo_stack.pop()
+        self.display()
+
+    def display(self):
+        print(self.document)
+
+def main():
+    print("Praise typing his body of knowldge")
+    word_processor = TextEditor("")
+    print("My blank slate")
+    word_processor.display()
+    word_processor.write("I")
+    word_processor.write(" ")
+    word_processor.write("k")
+    word_processor.write("m")
+    word_processor.undo()
+    word_processor.write("n")
+    word_processor.write("o")
+    word_processor.write("w")
+    print("Try undo and redo")
+    word_processor.undo()
+    word_processor.redo()
+main()
