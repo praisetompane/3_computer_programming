bc23d52976628168de12d15d15888342e8fe84d6
praisetompane <praisetompane@gmail.com>
2021-09-04 09:28:30 +0200

    basic django app and util scripts to use in future for creating Django applications

---
+++ frameworks_and_libraries/django/create_project.sh
@@ -0,0 +1,16 @@
+'''
+    accepts project name as parameter
+'''
+project=$1
+mkdir $project
+cp run_project.sh ./$project
+cd $project
+
+echo "creating virtual environment $project and installing Django"
+pipenv install django 
+echo "done"
+
+echo "creating Django project called $project"
+pipenv run django-admin startproject $project .
+echo "done"
+
